73f245081e7a4c2d2ff304335596ac6e936fe05d
Uilton <uilton_3001@outlook.com>
2023-09-27 15:10:55 -0300

    Feat:[method] Criando método de redirecionamento para o formulario de curso

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -5,6 +5,7 @@ import { Course } from '../model/course';
 import { CoursesService } from './../services/courses.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -19,7 +20,9 @@ export class CoursesComponent {
 
 constructor(
   private coursesService: CoursesService,
-  private dialog: MatDialog) {
+  private dialog: MatDialog,
+  private rounter: Router,
+  private route: ActivatedRoute) {
     this.courses$ = this.coursesService.list().pipe(
       catchError(error => {
         this.onError('Erro ao carregar.');
@@ -32,5 +35,9 @@ constructor(
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg,
     });
+  }
+
+  onAdd(){
+    this.rounter.navigate(['new'], {relativeTo: this.route});
   }
 }
