c14c62468e63c3d88d63bd9f5b4470a2f1102096
Uilton <uilton_3001@outlook.com>
2023-09-28 17:01:08 -0300

    Feat:[Add] Adicionando um método para voltar a anterior ao concluir ação

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,9 @@
-import { CoursesService } from './../services/courses.service';
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -14,7 +16,8 @@ export class CourseFormComponent {
 
   constructor(private formBuilder:FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar){
+    private snackBar: MatSnackBar,
+    private location: Location){
     this.form = this.formBuilder.group({
       name: [null],
       categoria: [null]
@@ -23,11 +26,16 @@ export class CourseFormComponent {
 
   onSubmit(){
     this.service.save(this.form.value)
-    .subscribe(data => console.log(data), error => this.onError());
+    .subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel(){
+    this.location.back();
+  }
 
+  private onSuccess(){
+    this.snackBar.open('Sucesso ao Salvar','',{duration: 3000});
+    this.location.back();
   }
 
   private onError(){
